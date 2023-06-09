21ad74c614e14fd0b132f2dc04a618eb0adfbed2
Morteza Baniadam <mortalbm@gmail.com>
2023-06-09 10:32:53 +0330

    added cdrom

--- main.tf
+++ main.tf
@@ -43,7 +43,7 @@ data "vsphere_host" "host" {
   name          = "esxi1"
   datacenter_id = data.vsphere_datacenter.dc.id
 }
-
+# current problem: vlan and vswitch cant be created at the same time 
 # // Create Virtual Switch
 # resource "vsphere_host_virtual_switch" "host_vswitch" {
 #   name           = var.vswitch_name
@@ -76,7 +76,7 @@ data "vsphere_host" "host" {
 
 // Create Virtual Machine
 resource "vsphere_virtual_machine" "vm" {
-  name                       = "${var.vm_name}-${count.index}"
+  name                       = "${var.vm_name}-master-${count.index+1}"
   resource_pool_id           = data.vsphere_resource_pool.pool.id
   datastore_id               = data.vsphere_datastore.datastore.id
   count                      = var.vm_count
@@ -91,7 +91,43 @@ resource "vsphere_virtual_machine" "vm" {
     network_id   = data.vsphere_network.vm_vlan.id
     adapter_type = var.adapter_type
   }
+  cdrom {
+    datastore_id = data.vsphere_datastore.datastore.id
+    path = "iso/ubuntu-22.04.2-live-server-amd64.iso"
+  }
+  disk {
+    label = "vm"
+    size  = var.vm_disksize
+  }
+  depends_on = [
+    # vsphere_host_virtual_switch.host_vswitch
+    data.vsphere_network.vm_vlan
+  ]
+}
 
+
+
+// Create Virtual Machine
+resource "vsphere_virtual_machine" "vm_worker" {
+  name                       = "${var.vm_name}-worker-${count.index+1}"
+  resource_pool_id           = data.vsphere_resource_pool.pool.id
+  datastore_id               = data.vsphere_datastore.datastore.id
+  count                      = var.vm_count
+  num_cpus                   = var.vm_cpu
+  memory                     = var.vm_ram
+  wait_for_guest_net_timeout = 0
+  // For more information about guest id in vmware >>>
+  // https://docs.vmware.com/en/VMware-HCX/4.6/hcx-user-guide/GUID-D4FFCBD6-9FEC-44E5-9E26-1BD0A2A81389.html
+  guest_id          = var.guest_id
+  nested_hv_enabled = true
+  network_interface {
+    network_id   = data.vsphere_network.vm_vlan.id
+    adapter_type = var.adapter_type
+  }
+  cdrom {
+    datastore_id = data.vsphere_datastore.datastore.id
+    path = "iso/ubuntu-22.04.2-live-server-amd64.iso"
+  }
   disk {
     label = "vm"
     size  = var.vm_disksize
